593b2816594ab27084afe74ccda3a771b4e00777
Cedric Robert <robert.ced@gmail.com>
2016-06-02 22:11:22 +0200

    chargement config mybatis

--- src/test/java/fr/crobert/AbstractTest.java
+++ src/test/java/fr/crobert/AbstractTest.java
@@ -2,16 +2,32 @@ package fr.crobert;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class AbstractTest {
 
     private DataSource dataSource;
+    private SqlSessionFactory sqlSessionFactory;
 
     AbstractTest() {
         HikariConfig config = new HikariConfig("hikari.properties");
         dataSource = new HikariDataSource(config);
+
+
+        String resource = "fr/crobert/mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
     }
 
 
